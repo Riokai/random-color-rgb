f32a8b15de46714cac0a2e4e3e35ecbaf37bf98f
Kai <zengkai2009@foxmail.com>
2016-03-24 15:46:26 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    filename: 'dist.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: "babel-loader" }
+    ]
+  }
+};
