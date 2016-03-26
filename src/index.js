6e84ba64a25da584183f5ee9ebef53db107e197b
Riokai <zengkai2009@foxmail.com>
2016-03-26 09:50:05 +0800

    remove comment

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ var getRandom = (max = 999999999999, min = 0) => {
   return min + Math.floor(Math.random() * (max - min))
 }
 
-// var getRandom = (max = 999999999999, min = 0) => {
-//   return min + Math.floor(Math.random() * (max - min))
-// }
-
 var getColor = (max = 255, min = 0) => {
   return 'rgb(' + getRandom(max, min) + ',' + getRandom(max, min) + ',' + getRandom(max, min) + ')'
 }
